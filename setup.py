d22dd4cadc72efd0cb8872e814f17a5bcf9b5a9b
Jason Murphy <jason@artuvic.com>
2013-06-19 20:32:34 -0500

    Changed README name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.ptg.bootstrapcarousel',
       version=version,
       description="Add Bootstraps Carousel to collective.plonetruegallery",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
